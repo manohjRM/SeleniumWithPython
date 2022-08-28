ccfce692e45a22a8e657c3df4b0ef5783931ff8d
manohjRM <manohjkumaar.rm@gmail.com>
2022-08-28 17:59:30 +0530

    update5

--- Resources/conftest.py
+++ Resources/conftest.py
@@ -1,7 +1,4 @@
 import pytest
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
 from Pages.keywordsClass import KeyClass
 from Resources.readProperties import ReadConfig
